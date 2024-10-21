beb480de03961e5bf04019387e2e34ffcc1ae8d3
MartinCharpentier92 <charpmartin@gmail.com>
2024-10-21 23:07:06 +0200

    Travaille encore autour du caroussel et de l'effe de dégradé dessus

--- JS/carousel.js
+++ JS/carousel.js
@@ -48,37 +48,72 @@
 
 
 
+// const carousel = document.querySelector(".carousel");
+// const firstImg = carousel.querySelectorAll("img")[0];
+// const arrowIcons = document.querySelectorAll(".wrapper i"); // Correction ici
+
+// let isDragStart = false, prevPageX, prevScrollLeft;
+// let firstImgWidth = firstImg.clientWidth + 14;
+
+// arrowIcons.forEach(icon => { // Utilisation correcte de forEach
+//     icon.addEventListener("click", () => {
+//         carousel.scrollLeft += icon.id === "left" ? -firstImgWidth : firstImgWidth;
+//     });
+// });
+
+// const dragStart = (e) => {
+//     isDragStart = true;
+//     prevPageX = e.pageX;
+//     prevScrollLeft = carousel.scrollLeft
+// }
+
+// const dragging = (e) => {
+//     if(!isDragStart) return;
+//     e.preventDefault();
+//     let positionDiff = e.pageX - prevPageX;
+//     carousel.scrollLeft = prevScrollLeft - positionDiff;
+// }
+
+// const dragStop = () => {
+//     isDragStart = false;
+// }
+
+// carousel.addEventListener("mousemove", dragging);
+// carousel.addEventListener("mousedown", dragStart);
+// carousel.addEventListener("mouseup", dragStop);
+
+
 const carousel = document.querySelector(".carousel");
-const firstImg = carousel.querySelectorAll("img")[0];
-const arrowIcons = document.querySelectorAll(".wrapper i"); // Correction ici
+    const firstImg = carousel.querySelectorAll("img")[0];
+    const arrowIcons = document.querySelectorAll(".wrapper i"); // Correction ici
 
-let isDragStart = false, prevPageX, prevScrollLeft;
-let firstImgWidth = firstImg.clientWidth + 14;
+    let isDragStart = false, prevPageX, prevScrollLeft;
+    let firstImgWidth = firstImg.clientWidth + 14;
 
-arrowIcons.forEach(icon => { // Utilisation correcte de forEach
-    icon.addEventListener("click", () => {
-        carousel.scrollLeft += icon.id === "left" ? -firstImgWidth : firstImgWidth;
+    arrowIcons.forEach(icon => { // Utilisation correcte de forEach
+        icon.addEventListener("click", () => {
+            carousel.scrollLeft += icon.id === "left" ? -firstImgWidth : firstImgWidth;
+        });
     });
-});
 
-const dragStart = (e) => {
-    isDragStart = true;
-    prevPageX = e.pageX;
-    prevScrollLeft = carousel.scrollLeft
-}
+    const dragStart = (e) => {
+        isDragStart = true;
+        prevPageX = e.pageX;
+        prevScrollLeft = carousel.scrollLeft;
+    }
 
-const dragging = (e) => {
-    if(!isDragStart) return;
-    e.preventDefault();
-    let positionDiff = e.pageX - prevPageX;
-    carousel.scrollLeft = prevScrollLeft - positionDiff;
-}
+    const dragging = (e) => {
+        if (!isDragStart) return;
+        e.preventDefault();
+        let positionDiff = e.pageX - prevPageX;
+        carousel.scrollLeft = prevScrollLeft - positionDiff;
+    }
 
-const dragStop = () => {
-    isDragStart = false;
-}
+    const dragStop = () => {
+        isDragStart = false;
+    }
 
-carousel.addEventListener("mousemove", dragging);
-carousel.addEventListener("mousedown", dragStart);
-carousel.addEventListener("mouseup", dragStop);
-
+    carousel.addEventListener("mousemove", dragging);
+    carousel.addEventListener("mousedown", dragStart);
+    carousel.addEventListener("mouseup", dragStop);
+    carousel.addEventListener("mouseleave", dragStop);

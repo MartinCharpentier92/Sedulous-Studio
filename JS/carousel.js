18144c3cd96343ae4268c0cb032ffc9abe850490
MartinCharpentier92 <charpmartin@gmail.com>
2024-12-23 13:46:40 +0100

    Working on animation

--- JS/carousel.js
+++ JS/carousel.js
@@ -117,3 +117,19 @@ const carousel = document.querySelector(".carousel");
     carousel.addEventListener("mousedown", dragStart);
     carousel.addEventListener("mouseup", dragStop);
     carousel.addEventListener("mouseleave", dragStop);
+
+
+
+let box =
+document.querySelector(".box");
+let observer =
+new IntersectionObserver(enteries => {
+    enteries.forEach(entry => {
+        if(entry.isIntersecting) {
+            entry.target.style.opacity = 1;
+            entry.target.style.transform = "translate(0)";
+        }
+    }) 
+})
+
+observer.observe(box)
